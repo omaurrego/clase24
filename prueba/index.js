2edacb09bf398547b5ee9bfbfceea97a82c6e1f1
omaurrego <99853272+omaurrego@users.noreply.github.com>
2022-03-24 19:36:31 -0500

    Add files via upload

---
+++ prueba/index.js
@@ -0,0 +1,44 @@
+function getMensaje(){
+    console.log("Hola grupo"); 
+    
+}
+
+const getNombre2 = () => {
+    console.log("Hola Grupo"); 
+
+}
+
+function sumar(){
+    const suma = (2+2); 
+    return suma;
+}
+
+const resultadoSuma = sumar();
+console.log(resultadoSuma)
+
+function multiplicar(){
+    const multiplicacion = (3*3);
+    return multiplicacion;
+}
+
+const resultadoMultiplicacion = multiplicar();
+console.log(resultadoMultiplicacion)
+
+function restar(){
+    const resta = (4-2);
+    return resta;
+}
+
+const resultadoResta = restar();
+console.log(resultadoResta)
+
+// function division(){
+//     const dividir = ()
+// }
+
+
+
+getMensaje();
+getNombre2();
+sumar();
+multiplicar();
